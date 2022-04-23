4ca1bcca1731cc1892ad59bfeecf825240fdaa47
mahadi54 <hmahadi505@gmail.com>
2022-04-23 22:26:31 +0600

    portfolio data update

--- src/PortfolioData/PortfolioData.js
+++ src/PortfolioData/PortfolioData.js
@@ -2,6 +2,15 @@ export default [
     {
         id: 1,
         port: 'portfolioModal1',
+        img: './assets/img/portfolio/personal-portfolio.png',
+        title: 'Personal Portfolio',
+        des: 'This is my personal portfolio site. This site is build with React js. Every section is design as reusable component. For the responsive design I use bootstrap. In this site, I showcase my details and my personal projects.',
+        live: 'https://mahadi54.netlify.app/',
+        github: 'https://github.com/mahadi54/react-personal-portfolio'
+    },
+    {
+        id: 2,
+        port: 'portfolioModal2',
         img: './assets/img/portfolio/React-linkedin-clone-img1.png',
         title: 'React Linkedin Clone',
         des: 'This is linkedin clone, created with popular javascript library React js. Their are some advance technology used here, such as: React Redux for state management, firebase for data store and authentication. Finally it deployed at netlify.',
@@ -9,8 +18,8 @@ export default [
         github: 'https://github.com/mahadi54/react-redux-linkedin-clone'
     },
     {
-        id: 2,
-        port: 'portfolioModal2',
+        id: 3,
+        port: 'portfolioModal3',
         img: './assets/img/portfolio/JS-DOM-Book-List.png',
         title: 'Javascript Book List App',
         des: 'This is a simple javascript book list app, created with js DOM. User can add their favourite books as well as they can delete books,',
@@ -18,8 +27,8 @@ export default [
         github: 'https://github.com/mahadi54/Book-List-App',
     },
     {
-        id: 3,
-        port: 'portfolioModal3',
+        id: 4,
+        port: 'portfolioModal4',
         img: '../../assets/img/portfolio/React-Weather-App.png',
         title: 'React Weather App',
         des: 'In this weather app, I used react js and for weather data openweather api is used. Also react useState and useEffect hooks is used.',
@@ -27,21 +36,15 @@ export default [
         github: 'https://github.com/mahadi54/react-weather-app'
     },
     {
-        id: 4,
-        port: 'portfolioModal4',
+        id: 5,
+        port: 'portfolioModal5',
         img: './assets/img/portfolio/react-todo-app.PNG',
         title: 'React ToDo App',
         des: 'This is a todo list app which is build with React js. Reak hooks is used. For the design part, I used Tailwind Css.',
         live: 'https://react-todo-app-mahadi54.netlify.app/',
         github: 'https://github.com/mahadi54/react-todo-app'
     },
-    {
-        id: 5,
-        port: 'portfolioModal5',
-        img: './assets/img/portfolio/safe.png',
-        title: 'Personal Portfolio',
-        des: 'This is my personal portfolio site. This site is build with React js. Every section is design as reusable component. For the responsive design I use bootstrap. In this site, I showcase my details and my personal projects.'
-    },
+    
     {
         id: 6,
         port: 'portfolioModal6',
